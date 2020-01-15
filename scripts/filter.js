7873a73cec5050eec63b3e09ead48f0121ca2a60
japi <japi@umich.edu>
2020-01-15 01:55:31 -0500

    3.0
    Huge update

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,15 +1,63 @@
-filterSelection("all")
+var prev_thumbnail_page = "page2";
+filterSelection("page1");
+//set default to page1
+
+
 function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("crop");
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-    if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
+    
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    let curr_thumbnails = document.getElementById(c);
+    
+    //if prev page is the same
+    if (c === prev_thumbnail_page) {
+        //do nothing
+    }
+    
+    else {
+        let thumbnails = curr_thumbnails.getElementsByClassName("proj-entry");
+        console.log(c);
+        curr_thumbnails.classList.add("page_show");
+        for (let h = 0; h < thumbnails.length; ++h) {
+            delaySwipeIn(h, thumbnails[h]);
+        }
+        
+        console.log(prev_thumbnail_page);
+        prev_thumbnails_cont = document.getElementById(prev_thumbnail_page);
+        console.log(prev_thumbnails_cont);
+        prev_thumbnails = prev_thumbnails_cont.getElementsByClassName("proj-entry");
+        
+        for (h = 0; h < prev_thumbnails.length; ++h) {
+            delaySwipeOut(h, prev_thumbnails[h], prev_thumbnails.length);
+        }
+        console.log(prev_thumbnails.length);
+        prev_thumbnails_cont.classList.remove("page_show"); 
+        
+        prev_thumbnail_page = c;
+        console.log(prev_thumbnail_page);
+    }
+
+      //w3AddClass(x[i], "page_show");
 }
 
+function delaySwipeIn(i, thumbnail) {
+    //thumbnail.classList.remove("swipe-out");
+    thumbnail.classList.add("show");
+    setTimeout(function() {
+        thumbnail.classList.add("swipe-in");
+    }, (i + 1) * 100);
+}
+function delaySwipeOut(i, thumbnail, len) {
+    setTimeout(function() {
+        //thumbnail.classList.add("swipe-out");
+        thumbnail.classList.remove("swipe-in");
+        
+    }, (i + 1) * 75);
+    setTimeout(function() {
+            thumbnail.classList.remove("show");
+    }, (len + 1) * 60);
+}
+
+/*
 // Show filtered elements
 function w3AddClass(element, name) {
   var i, arr1, arr2;
@@ -34,9 +82,9 @@ function w3RemoveClass(element, name) {
   }
   element.className = arr1.join(" ");
 }
-
+*/
 // Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("buttons");
+var btnContainer = document.getElementById("cat-menu");
 var btns = btnContainer.getElementsByClassName("btn");
 console.log(btns[0]);
 for (var i = 0; i < btns.length; i++) {
@@ -46,3 +94,75 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+//MENU FUNCTIONS
+
+var image_page = document.getElementById("menu-images");
+var about_page = document.getElementById("menu-about");
+about_page.style.display = "none";
+var work = document.getElementById("opt-work");
+var about = document.getElementById("opt-about");
+var arrow = document.getElementById("right-arrow");
+var background = document.getElementById("particles-js");
+var categories = document.getElementById("cat-menu");
+
+function catCascade(cascade) {
+    for (let i = 0; i < btns.length; ++i) {
+        if (cascade == true) {
+            btnContainer.style.display = "block";
+            catMoveDown(i, btns[i]);
+            console.log("CASCADE");
+            
+        }
+        else {
+            catMoveUp(i, btns[i]);
+            console.log("NO CASCADE");
+            setTimeout(function() {
+                btnContainer.style.display = "none";
+            }, 250);
+        }
+    }
+}
+
+function catMoveUp(i, btn) {
+    setTimeout(function() {
+        btn.classList.add("cat-hide");
+    }, i * 50);
+}
+function catMoveDown(i, btn) {
+    setTimeout(function() {
+        btn.classList.remove("cat-hide");
+    }, i * 50);
+}
+
+function image_open() {
+    background.classList.remove("back-blur");
+    arrow.style.transform = "translateX(15px)";
+    image_page.style.display = "grid";
+    about_page.style.display = "none";
+    about.classList.remove("active");
+    work.classList.add("active");
+    catCascade(true);
+}
+
+function image_close() {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    arrow.style.transform = "translateX(109px)";
+    background.classList.add("back-blur");
+    image_page.style.display = "none";
+    about_page.style.display = "grid";
+    work.classList.remove("active");
+    about.classList.add("active");
+    //categories.style.display = "none";
+    //close all modals when about
+    /*
+    var modal_container = document.getElementsByClassName("modal-container");
+    for (var i = 0; i < modal_container.length; ++i) {
+        close(modal_container[i]);
+    }
+    */
+    catCascade(false);
+}
+
+
--- scripts/pages.js
+++ scripts/pages.js
@@ -1,102 +1,225 @@
 
 // Get the <span> element that closes the modal
 var close;
-var modal;
-var all_modals = document.getElementsByClassName("modal-container");
-var images;
-var i = 0
+var curr_modal_header_gal;
+var i = 0;
 var filter = document.getElementById("modal-filter");
-var body = document.getElementById("body");
+var curr_modal;
+var intro;
+var sct;
+
+/*
+var temp = document.getElementsByClassName("crop");
+
+for (var k = 0; k != temp.length; ++k) {
+    var temp = document.getElementsByClassName("crop");
+    temp[k].addEventListener('click', function () {
+        this.classList.add('animate');
+        alert("expand");
+    });
+}
+*/
+
+function sct_back() {
+    close_modal(curr_modal.getAttribute('id'));
+}
+
+function updateSection(modal_name) {
+    //change section links
+    var introbtn = document.getElementById("intro-btn");
+    var processbtn = document.getElementById("process-btn");
+    var finalbtn = document.getElementById("final-btn");
+    
+    if (modal_name == "none") {
+        introbtn.href = "#intro-";
+        processbtn.href = "#process-";
+        finalbtn.href = "#final-";
+    }
+    else {
+        introbtn.href += modal_name;
+        processbtn.href += modal_name;
+        finalbtn.href += modal_name;
+    }
+}
+
 // When the user clicks on the button, open the modal 
 function open_modal(c) {
+    
+    //shrink menu bar
+    var name = document.getElementById("name");
+    setTimeout(()=> {
+        name.innerHTML = "JP";
+    }, 450);
+    
+    //hide menu opts
+    var menu = document.getElementsByClassName("menu-opts")[0]
+    menu.classList.add("menu-hide");
+    setTimeout(function() {menu.style.display= "none"}, 500);
+    
+    //set section names
+    //updateSection(c);
+    // get modal name
+    var sct_name = (c.split('-'))[0];
+    sct_name += "-sct";
+    sct = document.getElementById(sct_name);
+    
+    
+    
+    //show section btns
+    setTimeout(()=> {
+        sct.style.display = "block";
+    }, 400);
+    setTimeout(()=> {
+        sct.classList.add("sections-show");
+    }, 600);
+    
+    //hide cat menu
+    catCascade(false);
+    
+    //hide background
+    //document.getElementById("backcover").classList.remove("backcover_fade");
+    
+    cancelRequestAnimFrame(pJSDom[0].pJS.fn.checkAnimFrame);
+    cancelRequestAnimFrame(pJSDom[0].pJS.fn.drawAnimFrame);
+    pJSDom[0].pJS.fn.particlesEmpty();
+    pJSDom[0].pJS.fn.canvasClear();
+    
+    
     console.log("open");
-    modal = document.getElementById(c);
-    images = modal.getElementsByClassName("modal-crop");
-    i = 0;
+    curr_modal = document.getElementById(c);
+    intro = curr_modal.getElementsByClassName("intro")[0];
+    
+    //get images for the gallery, !!! needs revision
+    //images = modal.getElementsByClassName("modal-crop");
+    //i = 0;
+    
     //change display
-    modal.classList.add("modal-show");
+    curr_modal.classList.add("modal-show");
     //add transition
-    modal.classList.add("fade");
+    curr_modal.classList.add("fade");
     filter.classList.add("fade");
     
-    widthChange(w);
+    widthChange(w,);
+    
+    //if desktop
     if (w.matches) {
         setTimeout(function() {
             console.log("grow");
-            modal.classList.add("modal-opacity");
-            modal.classList.remove("modal-shrink");
+            curr_modal.classList.add("modal-opacity");
+            curr_modal.classList.remove("modal-shrink");
             filter.style.opacity = ".2";
             filter.style.zIndex = "2";
+            
         }, 10);
+        setTimeout(function() {
+            intro.classList.add("intro-expand-width");
+            setTimeout(function() {
+                intro.classList.add("intro-expand-height");
+            }, 500);
+        }, 100);
     }
+    //if mobile
     else {
-        modal.classList.remove("modal-shrink");
-        modal.classList.add("modal-opacity");
-        modal.classList.add("modal-swipe");
+        curr_modal.classList.remove("modal-shrink");
+        curr_modal.classList.add("modal-opacity");
+        curr_modal.classList.add("modal-swipe");
         setTimeout(function() {
             console.log("swipe");
-            modal.classList.remove("modal-swipe");
+            curr_modal.classList.remove("modal-swipe");
             filter.style.opacity = ".2";
             filter.style.zIndex = "2";
         }, 0);
+        setTimeout(function() {
+            intro.classList.add("intro-expand-width");
+            setTimeout(function() {
+            intro.classList.add("intro-expand-height");
+        }, 500);
+        }, 250);
     }
     
-   
-    
-    images[0].style.display = "block";
-    close = modal.getElementsByClassName("close")[0];
-    body.style.overflow = "hidden";   
+    //images[0].style.display = "block";
+    close = curr_modal.getElementsByClassName("close")[0];
+    document.body.style.overflow = "hidden";   
 }
 
 // When the user clicks on <span> (x), close the modal
 function close_modal(c) {
-    console.log("close");
-    widthChange(w);
-    modal = document.getElementById(c);
-    modal.classList.remove("modal-show");
-    modal.classList.remove("fade");
-    modal.classList.remove("modal-opacity");
-    if (w.matches) {
-        modal.classList.add("modal-shrink");
-    }
-    else {
-        modal.classList.add("modal-swipe");
-    }
+    //grow menu bar
+    var name = document.getElementById("name");
+    name.innerHTML = "JasonPi";
     
+    //show menu opts
+    var menu = document.getElementsByClassName("menu-opts")[0]
+    menu.style.display = "block";
+    setTimeout(function() {menu.classList.remove("menu-hide")}, 100);
+    
+    
+    //reset section names
+    //updateSection('none');
+    
+    //hide menu sections
+    sct.style.display = "none";
+    sct.classList.remove("sections-show");
+    
+    
+    catCascade(true);
+    intro.classList.remove("intro-expand-height");
+    intro.classList.remove("intro-expand-width");
+    console.log("close");
+    
+    //show background
+    //document.getElementById("backcover").classList.add("backcover_fade");
+    pJSDom[0].pJS.fn.vendors.start();
+    
+    //detect screen change
+    widthChange(w);
+    
+    curr_modal = document.getElementById(c);
+    curr_modal.classList.remove("modal-show");
+    curr_modal.classList.remove("fade");
+    curr_modal.classList.remove("modal-opacity");
+    
+    //if desktop
+    if (w.matches) {
+        curr_modal.classList.add("modal-shrink");
+    }
+    //if mobile
+    else {
+        curr_modal.classList.add("modal-swipe");
+    }
     
     filter.style.zIndex = "-1";
     filter.classList.remove("fade");
     filter.style.opacity = "0";
     
     
-    images[0].style.display = "none";
-    body.style.overflow = "auto";   
+    //images[0].style.display = "none";
+    document.body.style.overflow = "scroll";   
+    
 }
 
-var body = document.getElementById("body");
 // When the user clicks anywhere outside of the modal, close it
-filter.onclick = function(event) {
-    
-var modals = document.getElementsByClassName("modal-container");
-    close_modal(modal.getAttribute("id"));
+filter.onclick = function(event) {    
+    var modals = document.getElementsByClassName("modal-container");
+    close_modal(curr_modal.getAttribute("id"));
 }
 
 function img_right() {
-    images[i].style.display = "none";
-    if ((i + 1) == images.length) i = 0;
+    curr_modal_header_gal[i].style.display = "none";
+    if ((i + 1) == curr_modal_header_gal.length) i = 0;
     else {
         ++i
     }
-    images[i].style.display = "inline-block";
+    curr_modal_header_gal[i].style.display = "inline-block";
 }
 
 function img_left() {
-    images[i].style.display = "none";
-    if ((i - 1) == -1) i = images.length - 1;
+    curr_modal_header_gal[i].style.display = "none";
+    if ((i - 1) == -1) i = curr_modal_header_gal.length - 1;
     else {
         --i
     }
-    images[i].style.display = "inline-block";
+    curr_modal_header_gal[i].style.display = "inline-block";
 }
 const w = window.matchMedia("(min-width: 681px)");
 
@@ -108,35 +231,123 @@ if (matchMedia) {
 
 function widthChange(w) {
     console.log("width change");
+    var modals = document.getElementsByClassName("modal-container");
     //mobile to wide
     if (w.matches) {
-        for (var k = 0; k < all_modals.length; ++k) {
+        for (var k = 0; k < modals.length; ++k) {
             
             //hide inactive modals and reset animation
-            if (all_modals[k] != modal) {
-                all_modals[k].classList.add("modal-shrink");    
-                all_modals[k].classList.remove("modal-swipe");  
+            if (modals[k] != curr_modal) {
+                modals[k].classList.add("modal-shrink");    
+                modals[k].classList.remove("modal-swipe");  
             }
             else {
-                modal.classList.remove("modal-swipe");   
+                curr_modal.classList.remove("modal-swipe");   
             }
         }
         //change to gallery
-        for (var k = 0; k < images.length; ++k) {
-            images[k].style.display = "inline-block";
-        }
+        //for (var k = 0; k < images.length; ++k) {
+        //    images[k].style.display = "inline-block";
+        //}
     }
     //wide to mobile
     else {
-        for (var k = 0; k < all_modals.length; ++k) {
-            if (all_modals[k].id != modal.id) {
-                all_modals[k].classList.remove("modal-shrink"); 
-                all_modals[k].classList.add("modal-swipe"); 
+        for (var k = 0; k < modals.length; ++k) {
+            if (modals[k].id != curr_modal.id) {
+                modals[k].classList.remove("modal-shrink"); 
+                modals[k].classList.add("modal-swipe"); 
             }
         }
         //change images to carousel
-        for (var k = 1; k < images.length; ++k) {
-            images[k].style.display = "none";
+        //for (var k = 1; k < images.length; ++k) {
+        //    images[k].style.display = "none";
+        //}
+    }
+}
+
+
+
+//gallery functions
+
+var prev_gal_ind = 0;
+var gal = document.getElementById('gal-modal');
+var gal_img;
+var curr_gal;
+var display_img = document.getElementById('gal-img');
+
+//add event listeners to all modal-image
+var all_images = document.getElementsByClassName("modal-image");
+
+for (var i = 0; i != all_images.length; ++i) {
+    
+    //opens gal modal and changes image
+    all_images[i].addEventListener('click', function() {
+        //update images from chosen modal
+        gal_img = curr_modal.getElementsByClassName("modal-image");
+        //find ind of image
+        for (var k = 0; k != gal_img.length; ++k) {
+            if (this == gal_img[k]) prev_gal_ind = k;
         }
+        
+        display_img.src = this.src;
+        gal.style.display = "block";
+        
+        display_img_count();
+        
+    });
+}
+
+
+function gal_close() {
+    //gal_img[prev_gal_ind].style.display = "none";
+    prev_gal_ind = 0;
+    gal.style.display = "none";
+    
+}
+
+function gal_left() {
+    console.log("left");
+    
+    //if already at end of gal, loop
+    if (prev_gal_ind == 0) {
+        display_img.src = gal_img[gal_img.length - 1].src;
+        prev_gal_ind = gal_img.length - 1;
+    }
+    else {
+        display_img.src = gal_img[--prev_gal_ind].src;
+    }
+    display_img_count();
+}
+
+function gal_right() {
+    console.log("right");
+    
+    //if already at end of gal, loop
+    if (prev_gal_ind == (gal_img.length - 1)) {
+        display_img.src = gal_img[0].src;
+        prev_gal_ind = 0;
+    }
+    else {
+        display_img.src = gal_img[++prev_gal_ind].src;
+    }
+    display_img_count();
+}
+
+function display_img_count(){
+    document.getElementById("img-count").innerHTML = (+prev_gal_ind + 1).toString() + "/" + gal_img.length.toString();
+}
+
+//detect arrow press on gallery
+document.onkeydown = function(event) {
+    switch(event.keyCode) {
+        case 37:
+            gal_left();
+            break;
+        case 39:
+            gal_right();
+            break;
+        case 27:
+            gal_close();
+            break;
     }
 }
